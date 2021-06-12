86dbf54dbb801659ac3b143d3a74538eafc0c7fd
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-06-12 09:22:02 +0900

    CSS共通化

--- src/components/atoms/button/PrimaryButton.jsx
+++ src/components/atoms/button/PrimaryButton.jsx
@@ -1,19 +1,11 @@
 import styled from "styled-components";
+import { BaseButton } from "./BaseButton";
 
 export const PrimaryButton = (props) => {
   const { children } = props;
   return <SButton>{children}</SButton>;
 };
 
-const SButton = styled.button`
+const SButton = styled(BaseButton)`
   background-color: #40514e;
-  color: #fff;
-  padding: 6px 24px;
-  border: none;
-  border-radius: 9999px;
-  outline: none;
-  &:hover {
-    cursor: pointer;
-    opacity: 0.8;
-  }
 `;

f0b01a6cb6baa4c9759a0a4352ad4c727ba14b81
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-06-13 06:26:47 +0900

    organism骨組み作成

---
+++ src/components/organisms/user/UserCard.jsx
@@ -0,0 +1,18 @@
+export const UserCard = () => {
+  return (
+    <div>
+      <img alt="プロフィール" />
+      <p>名前</p>
+      <dl>
+        <dt>メール</dt>
+        <dd>test@test.com</dd>
+        <dt>TEL</dt>
+        <dd>090-1234-5678</dd>
+        <dt>会社名</dt>
+        <dd>てけ会社</dd>
+        <dt>WEB</dt>
+        <dd>teke.com</dd>
+      </dl>
+    </div>
+  );
+};

dc10c557ea5a1de74b240af02f5facf4a98c8d03
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-06-14 21:28:19 +0900

    pagesにheaderとfooter配置

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,16 +1,22 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { Top } from "../components/pages/Top";
 import { Users } from "../components/pages/Users";
+import { DefaultLayout } from "../components/templetes/DefaultLayout";
+import { HeaderOnly } from "../components/templetes/HeaderOnly";
 
 export const Router = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/">
-          <Top />
+          <DefaultLayout>
+            <Top />
+          </DefaultLayout>
         </Route>
         <Route path="/users">
-          <Users />
+          <HeaderOnly>
+            <Users />
+          </HeaderOnly>
         </Route>
       </Switch>
     </BrowserRouter>

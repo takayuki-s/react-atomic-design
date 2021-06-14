739b7ad5d68afee03e57870c7cf33efca326da36
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-06-14 21:44:33 +0900

    複製したUserCardをユーザー一覧に表示

--- src/App.js
+++ src/App.js
@@ -8,16 +8,16 @@
 import { Router } from "./router/Router";
 import "./styles.css";
 
-const user = {
-  name: "てけ",
-  image: "https://source.unsplash.com/oMpAz-DN-9I",
-  email: "test@test.com",
-  phone: "090-1234-5678",
-  company: {
-    name: "テスト株式会社"
-  },
-  website: "https://google.com"
-};
+// const user = {
+//   name: "てけ",
+//   image: "https://source.unsplash.com/oMpAz-DN-9I",
+//   email: "test@test.com",
+//   phone: "090-1234-5678",
+//   company: {
+//     name: "テスト株式会社"
+//   },
+//   website: "https://google.com"
+// };
 
 export default function App() {
   return (
--- src/components/pages/Users.jsx
+++ src/components/pages/Users.jsx
@@ -1,13 +1,36 @@
 import styled from "styled-components";
+import { SearchInput } from "../molecules/SearchInput";
+import { UserCard } from "../organisms/user/UserCard";
+
+const users = [...Array(10).keys()].map((val) => {
+  return {
+    id: val,
+    name: `てけ${val}`,
+    image: "https://source.unsplash.com/oMpAz-DN-9I",
+    email: "test@test.com",
+    phone: "090-1234-5678",
+    company: {
+      name: "テスト株式会社"
+    },
+    website: "https://google.com"
+  };
+});
 
 export const Users = () => {
   return (
     <SContainer>
       <h2>ユーザー一覧</h2>
+      <SearchInput />
+      {users.map((user) => (
+        <UserCard key={user.id} user={user} />
+      ))}
     </SContainer>
   );
 };
 
 const SContainer = styled.div`
-  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 24px;
 `;

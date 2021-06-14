1bce0ec39050081420ea1483667294e71a822146
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-06-14 09:19:59 +0900

    Cardコンポーネントに適用する共通CSS作成

--- src/components/organisms/user/UserCard.jsx
+++ src/components/organisms/user/UserCard.jsx
@@ -1,9 +1,10 @@
 import styled from "styled-components";
+import { Card } from "../../atoms/card/Card";
 
 export const UserCard = (props) => {
   const { user } = props;
   return (
-    <div>
+    <Card>
       <img height={160} width={160} alt={user.name} src={user.image} />
       <p>{user.name}</p>
       <SDl>
@@ -16,7 +17,7 @@ export const UserCard = (props) => {
         <dt>WEB</dt>
         <dd>{user.website}</dd>
       </SDl>
-    </div>
+    </Card>
   );
 };
 

c419ee68cadb12381c84976c61bd329346530c49
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-06-14 21:00:42 +0900

    Footer作成

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
 import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
 import { SearchInput } from "./components/molecules/SearchInput";
 import { UserCard } from "./components/organisms/user/UserCard";
+import { DefaultLayout } from "./components/templetes/DefaultLayout";
 import { HeaderOnly } from "./components/templetes/HeaderOnly";
 import "./styles.css";
 
@@ -20,13 +21,13 @@ const user = {
 export default function App() {
   return (
     <BrowserRouter>
-      <HeaderOnly>
+      <DefaultLayout>
         <PrimaryButton>テスト</PrimaryButton>
         <SecondaryButton>検索</SecondaryButton>
         <br />
         <SearchInput />
         <UserCard user={user} />
-      </HeaderOnly>
+      </DefaultLayout>
     </BrowserRouter>
   );
 }

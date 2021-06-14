e5f5ec34efeac1ffd896bc07a9dd198b5cae1780
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-06-14 21:23:44 +0900

    pagesのルーティング作成

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import { BrowserRouter } from "react-router-dom";
-import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
-import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
-import { SearchInput } from "./components/molecules/SearchInput";
-import { UserCard } from "./components/organisms/user/UserCard";
-import { DefaultLayout } from "./components/templetes/DefaultLayout";
-import { HeaderOnly } from "./components/templetes/HeaderOnly";
+// import { BrowserRouter } from "react-router-dom";
+// import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
+// import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
+// import { SearchInput } from "./components/molecules/SearchInput";
+// import { UserCard } from "./components/organisms/user/UserCard";
+// import { DefaultLayout } from "./components/templetes/DefaultLayout";
+// import { HeaderOnly } from "./components/templetes/HeaderOnly";
+import { Router } from "./router/Router";
 import "./styles.css";
 
 const user = {
@@ -20,14 +21,15 @@ const user = {
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <DefaultLayout>
-        <PrimaryButton>テスト</PrimaryButton>
-        <SecondaryButton>検索</SecondaryButton>
-        <br />
-        <SearchInput />
-        <UserCard user={user} />
-      </DefaultLayout>
-    </BrowserRouter>
+    // <BrowserRouter>
+    //   <DefaultLayout>
+    //     <PrimaryButton>テスト</PrimaryButton>
+    //     <SecondaryButton>検索</SecondaryButton>
+    //     <br />
+    //     <SearchInput />
+    //     <UserCard user={user} />
+    //   </DefaultLayout>
+    // </BrowserRouter>
+    <Router />
   );
 }

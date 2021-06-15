cd0fa0cfa984fa787c5ce6335d3472255382d3f9
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-06-15 09:15:33 +0900

    TOPページからUSERSページへ遷移するボタンを2つ用意（管理者、一般）

--- src/components/pages/Top.jsx
+++ src/components/pages/Top.jsx
@@ -1,9 +1,23 @@
 import styled from "styled-components";
+import { SecondaryButton } from "../atoms/button/SecondaryButton";
+import { useHistory } from "react-router-dom";
 
 export const Top = () => {
+  const history = useHistory();
+
+  const onClickAdmin = () => {
+    history.push("/users");
+  };
+  const onClickGeneral = () => {
+    history.push("/users");
+  };
   return (
     <SContainer>
       <h2>TOPページでっせ</h2>
+      <SecondaryButton onClick={onClickAdmin}>管理者ユーザー</SecondaryButton>
+      <br />
+      <br />
+      <SecondaryButton onClick={onClickGeneral}>一般ユーザー</SecondaryButton>
     </SContainer>
   );
 };
